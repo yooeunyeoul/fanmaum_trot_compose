07ddec0acc9a91bf375d59c6795ad36fc112d52a
choiyh2709 <yh.choi@fanmaum.com>
2022-05-26 17:46:07 +0900

    dagger hilt 및 basefragment 생성

--- app/src/main/java/com/trotfan/trot/BaseApplication.kt
+++ app/src/main/java/com/trotfan/trot/BaseApplication.kt
@@ -4,7 +4,7 @@ import android.app.Application
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
-class App : Application() {
+class BaseApplication : Application() {
     override fun onCreate() {
         super.onCreate()
     }
--- app/src/main/java/com/trotfan/trot/ui/MainActivity.kt
+++ app/src/main/java/com/trotfan/trot/ui/MainActivity.kt
@@ -3,7 +3,9 @@ package com.trotfan.trot.ui
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.trotfan.trot.R
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
