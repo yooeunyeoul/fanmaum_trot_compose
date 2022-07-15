e842b0afae02dd01db2d4b3b12ec2ca2443dd103
youeuneyol <ey.yoo@fanmaum.com>
2022-07-15 15:00:35 +0900

    프로젝트 컴포즈화

--- app/src/main/java/com/trotfan/trot/ui/MainActivity.kt
+++ app/src/main/java/com/trotfan/trot/ui/MainActivity.kt
@@ -1,14 +1,38 @@
 package com.trotfan.trot.ui
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.trotfan.trot.R
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
-class MainActivity : AppCompatActivity() {
+class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        setContent {
+            MaterialTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+                    Column {
+                        Text(
+                            text = "asdfasdfdasf",
+                            color = Color.Blue,
+                            style = MaterialTheme.typography.h6
+                        )
+                    }
+                }
+            }
+        }
+
     }
 }
