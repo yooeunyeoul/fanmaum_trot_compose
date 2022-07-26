8d5a8b96c4ce6cd1bc00cf59a4355c38c09a41b4
choiyh2709 <yh.choi@fanmaum.com>
2022-07-26 11:31:17 +0900

    [FWR-145] MainActivity import 제거

--- app/src/main/java/com/trotfan/trot/ui/MainActivity.kt
+++ app/src/main/java/com/trotfan/trot/ui/MainActivity.kt
@@ -3,10 +3,6 @@ package com.trotfan.trot.ui
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.unit.sp
-import androidx.core.view.WindowCompat
-import com.trotfan.trot.ui.theme.KotlinWorldFont
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
