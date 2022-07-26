f1e07f6871d651da081bcdb808c83cae54888674
choiyh2709 <yh.choi@fanmaum.com>
2022-07-26 18:01:04 +0900

    [FWR-129] Kakao Login SDK 연동

--- app/src/main/java/com/trotfan/trot/BaseApplication.kt
+++ app/src/main/java/com/trotfan/trot/BaseApplication.kt
@@ -1,8 +1,14 @@
 package com.trotfan.trot
 
 import android.app.Application
+import com.kakao.sdk.common.KakaoSdk
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
 class BaseApplication : Application() {
+    override fun onCreate() {
+        super.onCreate()
+
+        KakaoSdk.init(this, getString(R.string.kakao_native_app_key))
+    }
 }
--- app/src/main/java/com/trotfan/trot/ui/login/Login.kt
+++ app/src/main/java/com/trotfan/trot/ui/login/Login.kt
@@ -1,5 +1,6 @@
 package com.trotfan.trot.ui.login
 
+import android.util.Log
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.Surface
@@ -8,10 +9,12 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.kakao.sdk.user.UserApiClient
 import com.trotfan.trot.R
 import com.trotfan.trot.ui.login.components.LoginButton
 import com.trotfan.trot.ui.theme.*
@@ -19,6 +22,8 @@ import com.trotfan.trot.ui.utils.clickable
 
 @Composable
 fun LoginScreen() {
+    val context = LocalContext.current
+
     Surface(
         modifier = Modifier.fillMaxSize()
     ) {
@@ -33,7 +38,13 @@ fun LoginScreen() {
                 textColor = Gray800,
                 backgroundColor = Color(0XFFFEE500)
             ) {
-
+                UserApiClient.instance.loginWithKakaoTalk(context) { token, error ->
+                    if (error != null) {
+                        Log.e("Kakao Login", error.toString())
+                    } else if (token != null) {
+                        Log.e("Kakao Login", "로그인 성공 ${token.accessToken}")
+                    }
+                }
             }
             Spacer(modifier = Modifier.height(8.dp))
             LoginButton(
